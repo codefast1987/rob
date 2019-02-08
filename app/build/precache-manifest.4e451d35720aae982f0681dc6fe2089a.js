6f10483e627af4db4189379a09e2295d890af4bf
Taylor Kettle <tin.teapot@gmail.com>
2019-02-08 18:04:06 +1300

    added server

---
+++ app/build/precache-manifest.4e451d35720aae982f0681dc6fe2089a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22f1815fce24eac1c3ef",
+    "url": "/static/js/main.22f1815f.chunk.js"
+  },
+  {
+    "revision": "d2fcc3e2777842249318",
+    "url": "/static/js/1.d2fcc3e2.chunk.js"
+  },
+  {
+    "revision": "22f1815fce24eac1c3ef",
+    "url": "/static/css/main.46da15b0.chunk.css"
+  },
+  {
+    "revision": "d2fcc3e2777842249318",
+    "url": "/static/css/1.7ce5c04f.chunk.css"
+  },
+  {
+    "revision": "1eab7599a26c845f69ede482f51ab99e",
+    "url": "/index.html"
+  }
+];
