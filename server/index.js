e71ee2df5cda4b0428dca446ec9e3133cb455b7a
Taylor Kettle <tin.teapot@gmail.com>
2019-02-11 11:32:38 +1300

    changing server

--- server/index.js
+++ server/index.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static('app/build'));
 
 app.listen(process.env.PORT || 3000, (data) => {
-  console.log(`Listening on ${process.env.PORT}`);
+  console.log(`Listening on ${process.env.PORT || 3000}`);
 });
