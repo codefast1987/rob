c31fbb336199a4c5f4a97c9ab1c18c2a22b513e2
Preety <taylor@easyforms.co.nz>
2019-03-08 15:25:47 +1300

    changed About text

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -31,8 +31,7 @@ export default class Page extends Component {
         />
 
         <Section id="about" header="About">
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAApsa quae
           ab illo inventore veritatis et quasi architecto beatae vitae dicta
           sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
           aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
