18e45ea6f0f47549bb4885d9aefc2333e7658ee6
Preety Kaur <preetynz88@gmail.com>
2019-03-09 19:04:45 +1300

    supervisor installed

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -13,7 +13,7 @@ export default class Page extends Component {
     return (
       <div className="main-page">
         <Header>
-          <Title>After Hours Inspection</Title>
+          <Title>Robs Report</Title>
           <Menu>
             <a href="#about">About</a>
             <a href="#info">Info</a>
